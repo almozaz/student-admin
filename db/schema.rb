4176f7c914d5a7d4ff9308b004598c998514212d
almozaz <alacruzmozaz@gmail.com>
2017-09-18 13:57:39 +0200

    One to one relationship between user & profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918115036) do
+ActiveRecord::Schema.define(version: 20170918115454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,11 @@ ActiveRecord::Schema.define(version: 20170918115036) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.bigint "profile_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["profile_id"], name: "index_users_on_profile_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "users", "profiles"
 end
