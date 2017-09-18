05d74c16680b6a240fe79a9f9669f43c63f4bdb3
Jan-Sijmen <js.zwarts@gmail.com>
2017-09-18 15:12:21 +0200

    Adds api index for users

---
+++ app/controllers/api/users.controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+
+  def index
+    render status: 200, json: {
+      users: User.all
+    }.to_json
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
 
   resources :users
   resources :profiles
+
+  namespace :api do
+    resources :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
