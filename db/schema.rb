07128e922ec2c63c28dddc85a0debaec48341e1b
almozaz <alacruzmozaz@gmail.com>
2017-09-18 14:28:32 +0200

    User relationship & seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918115454) do
+ActiveRecord::Schema.define(version: 20170918115036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,8 +18,10 @@ ActiveRecord::Schema.define(version: 20170918115454) do
   create_table "profiles", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,11 +38,9 @@ ActiveRecord::Schema.define(version: 20170918115454) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
-    t.bigint "profile_id"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["profile_id"], name: "index_users_on_profile_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "users", "profiles"
+  add_foreign_key "profiles", "users"
 end
