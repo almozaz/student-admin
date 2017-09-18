e476090fea456b5e75e0883026865553963f3692
Jan-Sijmen <js.zwarts@gmail.com>
2017-09-18 13:45:42 +0200

    Adds test for user role

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "User roles" do
+    it "a new user is created as student by default"
+
+    student = User.new
+
+    expect(student.admin).to eq(false)
+  end
 end
