fbd4e033d93b31f30c6e4b9e3d7d112c54493c43
almozaz <alacruzmozaz@gmail.com>
2017-09-19 10:06:27 +0200

    Add calendar route & view file

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :profiles
+
+  get '/calendar' => 'application#calendar'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
