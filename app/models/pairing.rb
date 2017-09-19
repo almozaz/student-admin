e9a300c6dbb48b0fb23407f7d53883e146249652
almozaz <alacruzmozaz@gmail.com>
2017-09-19 17:32:02 +0200

    Pairing: logic structure

---
+++ app/models/pairing.rb
@@ -0,0 +1,29 @@
+class Pairing
+
+  def get_users
+    # get list of all users
+  end
+
+  def randomize_order
+
+  end
+
+  def remove_odd_one
+    # if number is odd, take one out & return value
+  end
+
+  def pair_users
+    # instanciate a match_pair for every valid pair
+    # pop instanciated users from list
+  end
+
+  def more_possibilities?
+    # check when no more possible matches
+    #
+  end
+
+  def upgrade_index
+    # match_pair.index ++
+  end
+
+end
