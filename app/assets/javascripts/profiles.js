e373bab706304d31040dfd9307253d2761fb6a2c
Jan-Sijmen <js.zwarts@gmail.com>
2017-09-19 10:53:36 +0200

    Fix to AJAX profile role code, now works after refreshing

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -9,13 +9,13 @@ function changeRole(event){
   var roleState = $(userItem).parent();
   var profileId = $(userItem).data("id");
 
-  profileAdmin = determineRole(profileId)
-  debugger;
+  profileAdmin = determineRole(userItem)
+
     $.ajax({
       type: "PATCH",
       url: "/api/profiles/"+profileId+".json",
       data: JSON.stringify({
-        admin: true
+        admin: profileAdmin
       }),
 
       contentType: "application/json",
@@ -23,9 +23,10 @@ function changeRole(event){
     })
 }
 
-function determineRole(profileId){
-
-  if ($(profileId).parent().prop("className") === "admin-list") {
+function determineRole(userItem){
+  var currentRole = $(userItem).parent().prop("className")
+  
+  if (currentRole == "admin-list") {
     return false;
   } else {
     return true;
