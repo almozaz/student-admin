4c980d2f3a42eb0feeba0ece4c2e4c8e237eb0ab
almozaz <alacruzmozaz@gmail.com>
2017-09-21 11:12:19 +0200

    Chain behaviours together

--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -2,33 +2,35 @@ class Pairing
 
   def initialize
     self.get_students
-
-    @pairs_per_round = @students.size / 2
-    @results = []
   end
 
   def get_students
     @students = Profile.where(admin: false).ids
-    @students.push(nil) if @students.size.odd?
   end
 
-  def create_pairs
+  def create_pairs(date)
+    @date = date
+    @students.push(nil) if @students.size.odd?
+    @pairs_per_round = @students.size / 2
+    @results = []
+
     @pairs_per_round.times do |index|
       @results << [@students[index], @students.reverse[index]]
     end
+    self.match_pairs
   end
 
   def match_pairs
     @results.each do |pair|
       student = pair[0]
       match = pair[1]
-      MatchPair.create(user_id: student, match_id: match)
+      MatchPair.create(user_id: student, match_id: match, date: @date)
     end
+    self.rotate
   end
 
   def rotate
     @students = [@students[0]] + @students[1..-1].rotate(-1)
     @results = []
   end
-
 end
