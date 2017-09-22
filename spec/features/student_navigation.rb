f511ec1149dba3b61cd7fe9ff7718cccdda38299
Jan-Sijmen <js.zwarts@gmail.com>
2017-09-22 13:15:09 +0200

    Pages controller works properly

--- spec/features/student_navigation.rb
+++ spec/features/student_navigation.rb
@@ -1,14 +1,13 @@
 require 'rails_helper'
 
 describe "Navigation" do
-  before do
+
+  let!(:student)         { create :user }
+  let!(:student_profile) { create :profile, admin: false, user: student }
+
+  it "shows today's match" do
     login_as student
-  end
 
-  let(:student) { create :user }
-  let(:student_profile) { create :profile, user: student }
-
-  it "can only see calendar" do
     visit root_path
 
     expect(page).to have_text("Your match for today is:")
